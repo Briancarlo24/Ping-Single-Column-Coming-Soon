11fc760db3db08cd23413042fabcec0590fcabc6
Briancarlo24 <Briancarlo2494@gmail.com>
2021-09-18 13:18:10 +0800

    challenge completed. Finished Design and functionality

---
+++ script.js
@@ -0,0 +1,37 @@
+const notifyButton = document.querySelector("button");
+const inputField = document.querySelector(".email");
+const errorMessage = document.querySelector(".popup-error");
+init();
+
+notifyButton.addEventListener("click", () => {
+  console.log("Validating Email");
+  validate();
+});
+
+function init() {
+  inputField.classList.remove("borderChange");
+  errorMessage.classList.add("hidden");
+}
+
+function validate() {
+  let email = document.getElementById("email").value;
+  if (validateEmail(email) && email !== "") {
+    alert("email " + email + " is valid");
+    inputField.value = "";
+    window.location.reload();
+
+    return true;
+  } else {
+    inputField.classList.add("borderChange");
+    errorMessage.classList.remove("hidden");
+    inputField.setAttribute("placeholder", "example@email/com");
+    return false;
+  }
+}
+
+// To Check the validiy of the email Address
+function validateEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
